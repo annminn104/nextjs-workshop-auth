dcafafb1051e9db6bd202b1138e7ef8645d3294c
Anh Minh <nguyencaoanhminh@amitgroup.vn>
2022-07-06 15:43:28 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        : "https://nextjs-workshop-api-team3.vercel.app/api", // production api
   },
 };
